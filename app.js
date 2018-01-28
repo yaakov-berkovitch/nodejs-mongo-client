6d3a5baabaaf890fe036acb683bf08f41f3b5202
Yaakov Berkovitch <yaakov.berkovitch@gmail.com>
2018-01-28 15:05:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+'use strict';
+
 //Import the mongoose module
 var mongoose = require('mongoose');
-//
+var os = require('os');
+const express = require('express')
+const app = express()
+const servername = process.env.MONGODB_SERVER_NAME ;
+const listenPort = process.env.LISTEN_PORT ;
+
 //Set up default mongoose connection
-var mongoDB = 'mongodb://epic_mongodb:27017/paas-auth-db';
+var mongoDB = 'mongodb://' + servername + ':27017/paas-auth-db';
 //var options = { replicaSet: 'rs0', useMongoClient: true};
 var options = { replicaSet: 'rs0', useMongoClient: false};
 
@@ -14,35 +21,77 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.on('connected', () => {console.info('Connected to MongoDB!');});
+db.on('error', console.error.bind(console, 'MongoDB connection error to server = ', servername));
+db.on('connected', () => {console.info('Connected to MongoDB server = ', servername);});
 
 // Define a schema
 var userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, lowercase: true },
-  password: String,
-  creationtime: Date,
+	email: { type: String, unique: true, lowercase: true },
+	password: String,
+	creationtime: Date,
 });
 
 var user = mongoose.model('users', userSchema);
 
-// find all users 
-user.find({ }, function (err, users) {
-	if (err) return handleError(err);
-	// 'users' contains the list of users
-	console.log('User list = ' + users)
-})
+class UserManagement {
+	constructor() {
+		if (! UserManagement.instance) {
+			UserManagement.instance = this;
+			console.log('Instantiate UserManagement');
+		}
+		return UserManagement.instance;
+	}
 
-// find specific user
-user.find({'email' : 'user8@gmail.com'}, function (err, foundUser) {
-	if (err) return handleError(err);
-	// 'users' contains the user retrieved from database
-	console.log('Found one user = ' + foundUser)
-})
+	// find all users
+	async getAllUsers () {
+		try {
+			const users = await user.find({}).exec();
+			console.log('getAllUsers - success - users = ', users)
+			return users ;
+		} catch (err) {
+			console.log('getAllUsers - exception occurred = ', err)
+			return undefined ;
+		}
+	}
 
-setTimeout (function() {console.log('Terminating'); process.exit(0);}, 10000) ;
+	// find specific user
+	async getUser (username) {
+		try {
+			const dbuser = await user.find({'email' : username}) ;
+			// 'foundUser' contains the user retrieved
+			console.log('getUser - success - user = ', dbuser)
+			return dbuser ;
+		} catch (err) {
+			console.log('getUser - exception occurred = ', err)
+			return undefined ;
+		}
+	}
 
-function handleError(error) {
-	console.log('Error handling - error = ', error);
+	handleError(error) {
+		console.log('Error handling - error = ', error);
+	}
 }
+const userManagerWrapperInstance = new UserManagement();
+
+app.get('/', (req, res) => res.send('Hello World - Server = ' + os.hostname() + '\n'))
+app.get('/users', async (req, res) => {
+	try {
+		const users = await userManagerWrapperInstance.getAllUsers();
+		console.log('User list = ', users)
+		res.json(users)
+	} catch (err) {
+		res.status(500).send(err)
+	}
+})
+app.get('/user/:username', async (req, res) => {
+	try {
+		const user = await userManagerWrapperInstance.getUser(req.params.username);
+		console.log('User found = ', user)
+		res.json(user)
+	} catch (err) {
+		res.status(500).send(err)
+	}
+})
+
+app.listen(listenPort, () => console.log('Example app listening on port ', listenPort))
 
